a6bf790dc95689bf211cadbb602124dbb0484616
sashka10 <sashka.333@yandex.ru>
2021-05-30 02:32:41 +0300

    design is now supported

--- main.py
+++ main.py
@@ -51,7 +51,6 @@ def select_tracks():
         else:
             Track_box.delete(0, END)
         current_tracks_path = (path.split(selected_tracks[0]))[0]
-        print("---" + current_tracks_path)
         for track_title in selected_tracks:
             track_title = (track_title.split('/'))[-1]
             Track_box.insert(END, track_title)
@@ -67,7 +66,7 @@ def play():
     global current_track_title
     global current_tracks_path
     is_playing = True
-    Pause_resume_button.config(text="=", command=pause)
+    Pause_resume_button.config(text="pause", command=pause)
     try:
         mixer.music.play(start=Music_Slider.get())
         update_music_slider_position()
@@ -76,7 +75,7 @@ def play():
 
 
 def replay():
-    Pause_resume_button.config(text="=", command=pause)
+    Pause_resume_button.config(text="replay", command=pause)
     Music_Slider.set(0)
     play()
 
@@ -85,7 +84,7 @@ def pause():
     global is_playing
     global after_id
     is_playing = False
-    Pause_resume_button.config(text="^", command=play)
+    Pause_resume_button.config(text="play", command=play)
     try:
         Time_passed_label.after_cancel(after_id)
         mixer.music.stop()
@@ -221,7 +220,7 @@ def manipulate_track_time(curr_time):
             play_selected_track()
             return
     elif Music_Slider.get() == track_duration:
-        Pause_resume_button.config(text="↺", command=replay)
+        Pause_resume_button.config(text="replay", command=replay)
         Time_passed_label.after_cancel(after_id)
         is_playing = False
     if is_playing:
@@ -254,7 +253,7 @@ def slideOfmusicSlider(x):
             play_selected_track()
             return
     elif int(x) == track_duration:
-        Pause_resume_button.config(text="↺", command=replay)
+        Pause_resume_button.config(text="replay", command=replay)
         Time_passed_label.after_cancel(after_id)
         is_playing = False
 
@@ -281,12 +280,21 @@ master.resizable(False, False)
 
 # Images
 
+play_img = PhotoImage(file='Source images/play_button.png')
+pause_img = PhotoImage(file='Source images/pause_button.png')
+replay_img = PhotoImage(file='Source images/replay_button.png')
+skip_time_button_f_img = PhotoImage(file='Source images/skip_time_button.gif')
+skip_time_button_img = PhotoImage(file='Source images/skip_time_button_left.gif')
+next_track_button_img = PhotoImage(file='Source images/next_track.gif')
+prev_track_button_img = PhotoImage(file='Source images/prev_track.gif')
+
+
 # Creating and packing frames
 
 Main_bottom_frame = Frame(master, bg='#ffffff')
 Main_bottom_frame.pack(side=BOTTOM, pady=10)
 
-Volume_slider_frame = Frame(Main_bottom_frame)
+Volume_slider_frame = Frame(Main_bottom_frame, bg="#ffffff")
 Volume_slider_frame.pack(fill=X)
 
 Track_being_played = Label(master, bg="#ffffff")
@@ -295,74 +303,98 @@ Track_being_played.pack(side=BOTTOM)
 Control_buttons_frame = Frame(Main_bottom_frame, bg='#ffffff')
 Control_buttons_frame.pack(side=BOTTOM)
 
-Track_time_frame = Frame(Main_bottom_frame)
+Track_time_frame = Frame(Main_bottom_frame, bg="#ffffff")
 Track_time_frame.pack(fill=X, side=BOTTOM)
 
-Playlist_frame = Frame(master)
+Playlist_frame = Frame(master,bg="#ffffff")
 Playlist_frame.pack(fill=X, padx=20)
 
 
 # Play list menu
 
-Playlist_scrollbar = Scrollbar(Playlist_frame)
-Track_box = Listbox(Playlist_frame,selectbackground="#C0C0C0", bd=5, fg="#000000", selectmode=SINGLE, bg='purple', width=100, yscrollcommand=Playlist_scrollbar.set)
+#Playlist_scrollbar = Scrollbar(Playlist_frame, troughcolor="#ffffff", bg="#000000", activebackground="#0F031D")
+new_slider = Scale(Playlist_frame, length=150,showvalue=0, activebackground="#000000" ,troughcolor="#ffffff", bg="#000000",from_=0, to=10, resolution=1, orient = VERTICAL)
+Track_box = Listbox(Playlist_frame,selectbackground="#C0C0C0", bd=5, fg="#000000", selectmode=SINGLE, bg='white', width=100)#, yscrollcommand=new_slider.set)
 Track_box.bind('<<ListboxSelect>>', show_action)
-Playlist_scrollbar.config(command=Track_box.yview)
+new_slider.config(command=Track_box.yview)
+#Playlist_scrollbar.config(command=Track_box.yview)
 
-#
+
+# Checkbox
 
 autoplay_checkbox = Checkbutton(master, text="Autoplay", bg="#ffffff",variable=autoplay, onvalue=True, offvalue=False, command=enable_autoplay)
 autoplay_checkbox.pack()
 
 # Buttons
 
-Select_track_button = Button(Playlist_frame, text="Select tracks", command=select_tracks)
-Select_new_track_button = Button(Playlist_frame, text="Play selected", command=play_selected_track)
+Select_track_button = Button(Playlist_frame, bg="#000000", fg="#ffffff", activebackground="#000000", bd=7,
+                             width=40, height=1,
+                             text="Select tracks", font="Verdana 10", command=select_tracks)
 
-Skip_forward_button = Button(Control_buttons_frame, text=">>", command=skip_forward)
-Skip_backwards_button = Button(Control_buttons_frame, text="<<", command=skip_backwards)
 
-Next_track_button = Button(Control_buttons_frame, text=">", command=next_track)
-Previous_track_button = Button(Control_buttons_frame, text="<", command=prev_track)
+Select_new_track_button = Button(Playlist_frame, bg="#000000", fg="#ffffff", activebackground="#000000", bd=7,
+                             text="play selected", command=play_selected_track)
 
-Pause_resume_button = Button(Control_buttons_frame, text="^", command=play)
+Skip_forward_button = Button(Control_buttons_frame, bg="#000000", fg="#ffffff", activebackground="#000000", bd=7,
+                             width=6, height=1,
+                             text=">>", font="Verdana 10", command=skip_forward)
+
+Skip_backwards_button = Button(Control_buttons_frame, bg="#000000", fg="#ffffff", activebackground="#000000", bd=7,
+                             width=6, height=1,
+                             text="<<", font="Verdana 10", command=skip_backwards)
+
+Next_track_button = Button(Control_buttons_frame, bg="#000000", fg="#ffffff", activebackground="#000000", bd=7,
+                             width=6, height=1,
+                             text=">", font="Verdana 10", command=next_track)
+
+Previous_track_button = Button(Control_buttons_frame, bg="#000000", fg="#ffffff", activebackground="#000000", bd=7,
+                             width=6, height=1,
+                             text="<", font="Verdana 10", command=prev_track)
+
+Pause_resume_button = Button(Control_buttons_frame, bg="#000000", fg="#ffffff", activebackground="#000000", bd=7,
+                             width=6, height=1, font="Verdana 10",
+                             text="play",command=play)
 
 
 # Sliders
 
-Music_Slider = Scale(Main_bottom_frame, state=DISABLED, orient="horizontal", showvalue=False, length=330, from_=0, resolution=1, repeatdelay=0, command=slideOfmusicSlider)
+Music_Slider = Scale(Main_bottom_frame, state=DISABLED, orient="horizontal",
+                     troughcolor="#ffffff", bg="#000000", resolution=1, repeatdelay=1, showvalue=False,
+                     length=330, from_=0, activebackground="#000000", command=slideOfmusicSlider)
 Music_Slider.bind('<ButtonRelease-1>', manipulate_track_time)
 Music_Slider.bind('<ButtonPress-1>', stop_while_manipulating)
 
-Volume_Slider = Scale(Volume_slider_frame, orient="horizontal", showvalue=False ,length=80, from_=0, to=1, resolution=0.01, command=manipulate_volume)
+Volume_Slider = Scale(Volume_slider_frame, orient="horizontal", bg="#000000", troughcolor="#ffffff",
+                      showvalue=False, activebackground="#000000",
+                      length=80, from_=0, to=1, resolution=0.01, command=manipulate_volume)
 Volume_Slider.set(current_volume)
 Volume_Slider.pack(side=RIGHT)
 
 # Labels
 
-Track_length_hms_label = Label(Track_time_frame, text="0:00:00")
+Track_length_hms_label = Label(Track_time_frame, bg="#ffffff", text="0:00:00")
 
-Time_passed_label = Label(Track_time_frame, text="0:00:00")
+Time_passed_label = Label(Track_time_frame, bg="#ffffff", text="0:00:00")
 
 # Packing widgets into frames
 Select_track_button.pack(pady=10)
 
-Playlist_scrollbar.pack(side=RIGHT,fill=Y)
+#Playlist_scrollbar.pack(side=RIGHT,fill=Y)
+new_slider.pack(side=RIGHT, fill=Y)
 
 Track_box.pack(padx=20)
 Select_new_track_button.pack(fill=X, padx=20)
 
 Music_Slider.pack(side=TOP)
 
-Previous_track_button.pack(side=LEFT, padx=10)
-Skip_backwards_button.pack(side=LEFT, padx=10)
-Pause_resume_button.pack(side=LEFT, padx=10)
-Skip_forward_button.pack(side=LEFT,padx=10)
-Next_track_button.pack(side=LEFT, padx=10)
+Previous_track_button.pack(side=LEFT)
+Skip_backwards_button.pack(side=LEFT)
+Pause_resume_button.pack(side=LEFT)
+Skip_forward_button.pack(side=LEFT)
+Next_track_button.pack(side=LEFT)
 
-#Volume_Slider.pack(side=RIGHT,padx=20, ipady=6)
 
 Time_passed_label.pack(side=LEFT)
 Track_length_hms_label.pack(side=RIGHT)
 
-master.mainloop()  # we need to use this function, in order to constantly keep our window ready
+master.mainloop()  # we need to use this method, in order to constantly keep our window ready
